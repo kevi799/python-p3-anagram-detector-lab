a2fbef2d679c4a8abfa2df1eb275b8d5b7fa6ce7
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-12-03 20:05:56 +0300

    working with anagrm detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, list_anagram):
+        matches = []
+        for word in list_anagram:
+
+            if ''.join(sorted(word.lower())) == self.sorted_word and word.lower() != self.word:
+                matches.append(word)
+        return matches
+    
+pikachu = Anagram('listen')
+words = ["enlists", "google", "inlets", "banana"]
+print(pikachu.match(words))
+
